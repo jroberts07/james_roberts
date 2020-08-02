6f1b917569b0f09041a3ecb22d3f108d1ea25bb5
James Roberts <j.roberts07@outlook.com>
2020-08-02 20:17:52 +0100

    Send mail

--- wsgi.py
+++ wsgi.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
     app.wsgi_app = SassMiddleware(app.wsgi_app, {
         'web_server': ('static/assets/sass', 'static/assets/css', '/static/assets/css')
     })
+
     app.run()
